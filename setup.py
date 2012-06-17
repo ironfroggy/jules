93492511d94c4571fe75884c49ea18be62d6e1f9
Calvin Spealman <calvin@pantechnoco.com>
2012-06-17 10:35:32 -0400

    Basic setup.py and an empty package.

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+INSTALL_REQUIRES = [
+    'straight.plugin',
+    'straight.command',
+]
+
+setup(name='jules',
+    version='0.1-alpha-1',
+    description='Yet Another Static Blog Generator I guess',
+    author='Calvin Spealman',
+    author_email='ironfroggy@gmail.com',
+    packages=['jules'],
+    install_requires=INSTALL_REQUIRES,
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Environment :: Plugins',
+    ]
+)
