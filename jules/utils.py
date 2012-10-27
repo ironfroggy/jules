8cfbf852f22e6bacdae10482275a24343452a4ac
Calvin Spealman <calvin@pantechnoco.com>
2012-10-27 13:40:52 -0400

    Use range() instead of xrange() for python 3

--- jules/utils.py
+++ jules/utils.py
@@ -5,7 +5,7 @@ from straight.plugin import load
 
 def ensure_path(path):
     path_parts = os.path.split(path)
-    for i in xrange(len(path_parts)):
+    for i in range(len(path_parts)):
         subpath = os.path.join(*path_parts[: i + 1 ])
         if subpath and not os.path.exists(subpath):
             os.mkdir(subpath)
